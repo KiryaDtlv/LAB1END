ede642231dafa9370ee536d6a85b5a04970e896a
KiryaDtlv <dyatlov.kiryuha@gmail.com>
2020-05-13 16:46:00 +0700

    Add Math.c

---
+++ src/Math.c
@@ -0,0 +1,15 @@
+int Perimetr(int r)
+{
+    if (r <= 0) {
+        return 1;
+    } else
+        return 0;
+}
+
+int Ploshad(int r)
+{
+    if (r <= 0) {
+        return 1;
+    } else
+        return 0;
+}
