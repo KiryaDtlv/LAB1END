f3341c72037cce6c5e0df2a0c2174b6604c66b5c
KiryaDtlv <dyatlov.kiryuha@gmail.com>
2020-05-16 16:29:47 +0700

    Math.c

--- src/Math.c
+++ src/Math.c
@@ -4,10 +4,10 @@ int PS(int r)
 {
     float S;
     if (r <= 0) {
-       printf("Неверный радиус!");
-        return 1;
+         printf("Неверный радиус!");
+         return 1;
     } else
-        S = p * r * r;
+         S = p * r * r;
     printf("Площадь %0.2f", S);
     float P = 2 * p * r;
     printf("Периметр %0.2f", P);
